d489351244b8cce3bc83579f3a152110cbe39281
fln00b13 <38315458+fln00b13@users.noreply.github.com>
2021-06-10 15:05:59 +0800

    Update windows10-wifi.py

--- windows10-wifi.py
+++ windows10-wifi.py
@@ -1,22 +1,7 @@
-#    Import subprocess so we can use system commands.
 import subprocess
 
-#    Import the re module so we can make use of regular expressions. 
 import re
 
-#    Python allows us to run system commands using the function 
-#    provided by the subprocess module; 
-#    (subprocess.run(<list of command line arguments go here>, <specify the second argument if you want to capture the output>)).
-#
-#    This script is a parent process that creates a child process which 
-#    runs a system command and will only continue once the child process 
-#    is completed. 
-#
-#    To save the contents that get sent to the standard output stream 
-#    (the terminal), we must first specify that we want to capture the output.
-#    To do this we specify the second argument as capture_output = True. 
-#    This information gets stored in the stdout attribute as bytes and 
-#    needs to be decoded before being used as a String in Python.
 command_output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
 
 profile_names = (re.findall("All User Profile     : (.*)\r", command_output))
@@ -40,4 +25,4 @@ if len(profile_names) != 0:
             wifi_list.append(wifi_profile) 
 
 for x in range(len(wifi_list)):
-    print(wifi_list[x]) 
+    print(wifi_list[x])
